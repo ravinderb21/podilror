0968ba04e4a12fc70ee726d5a2469b6f43104383
Ravinder Bhatia <ravinderb21@icloud.com>
2017-03-19 21:36:03 -0700

    building user registrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
 
   layout 'user'
+
+  def index
+
+  end
   
   def new
     @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(users_path)
+    else
+      flash[:notice] = "Registration unsuccessful!"
+      render('new')
+    end
   end
 
   def edit
@@ -16,5 +27,11 @@ class UsersController < ApplicationController
   end
 
   def deactivate
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
 
 	has_many :images
 
+	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
+	validates :email, :presence => true,
+										:length => { :maximum => 100 },
+										:format => EMAIL_REGEX,
+										:uniqueness => true
+
 end
