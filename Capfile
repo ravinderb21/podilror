8de91daf0ac35a0b1d3076e56a411d0109da2026
Ravinder Bhatia <ravinderb21@icloud.com>
2017-04-09 11:30:29 -0700

    fix ruby version and minor changes for produciton deploy

--- Capfile
+++ Capfile
@@ -28,11 +28,11 @@ install_plugin Capistrano::SCM::Git
 
 require "capistrano/rbenv"
 set :rbenv_type, :user
-set :rbenv_ruby, '2.4.0'
+set :rbenv_ruby, '2.4.1'
 
 require "capistrano/bundler"
 require "capistrano/rails"
-require "capistrano/passenger"
+# require "capistrano/passenger"
 # 
 # require "capistrano/rvm"
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+set :stage, :production
+server '104.198.96.6', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
