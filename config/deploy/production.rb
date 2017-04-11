f2f6ff185f38cc4a35a50683712ef2ba7b6a5f87
Ravinder Bhatia <ravinder@ilscorp.com>
2017-04-11 07:17:59 -0700

    updated new server IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :stage, :production
-server '104.198.96.6', user: 'deploy', roles: %w{app db web}
+server '35.184.149.246', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
