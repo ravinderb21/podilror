11bd6497f98c065acc0bf57c82460a432b451690
Ravinder Bhatia <ravinder@ilscorp.com>
2017-04-11 07:25:26 -0700

    passenger restart with touch

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,15 @@ lock "3.8.0"
 set :application, "podilror"
 set :repo_url, "git@github.com:achzodiac/podilror.git"
 
-set :deploy_to, '/home/deploy/podilror'
+set :passenger_restart_with_touch, true
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+
+set :deploy_to, '/home/deploy/podilror'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
