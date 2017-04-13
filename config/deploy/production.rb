b94634965ab5b55d68f7c0d61e6bdd6eabbe9f52
Ravinder Bhatia <ravinder@ilscorp.com>
2017-04-13 14:47:52 -0700

    updating app with new server ip and also deploying without capistrano passenger gem for now

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :stage, :production
-server '35.184.149.246', user: 'deploy', roles: %w{app db web}
+server '35.185.209.222', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
